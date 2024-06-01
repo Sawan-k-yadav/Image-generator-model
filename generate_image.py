533d25368a8533bfbc19b143329fa89509b0dd18
Sawan-k-yadav <sawan.gomia@gmail.com>
2024-06-02 00:01:10 +0530

    Adding my all project files

--- generate_image.py
+++ generate_image.py
@@ -3,9 +3,7 @@ import os
 import openai
 
 # Replace with your OpenAI API key
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-
-openai.api_key = "sk-proj-6iSZnIijtets51wEkCNwT3BlbkFJ6wBtEnb22T3NVaGLgkC3"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 app = Flask(__name__)
